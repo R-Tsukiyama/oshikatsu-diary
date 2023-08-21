8e6cb9e14361ded545fecb0503c1570dd93afa60
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-08-21 16:10:46 +0900

    [add]Add homepage and loginpage

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,18 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus   = application;
 
-export { application }
+export { application };
+
+
+import jquery from "jquery"
+window.$ = jquery
+
+$(".hamburger-menu").click(function () {
+  $(this).toggleClass('active');
+  $(".navbar-list").toggleClass('show');
+});
